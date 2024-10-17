8036209cd38eb94267aa39b27353585175dd793b
shnjnmkkr <84410852+shnjnmkkr@users.noreply.github.com>
2024-10-17 13:03:47 +0530

    Update letter_counter_single_thread.py
    Can now count the frequencies of all characters, not just alphabets.

--- letter_frequencies/letter_counter_single_thread.py
+++ letter_frequencies/letter_counter_single_thread.py
@@ -10,17 +10,18 @@ def count_letters(url, frequency):
         letter = l.lower()
         if letter in frequency:
             frequency[letter] += 1
+        else:
+            frequency[letter]=0
 
 
 def main():
     frequency = {}
-    for c in "abcdefghijklmnopqrstuvwxyz":
-        frequency[c] = 0
     start = time.time()
     for i in range(1000, 1020):
         count_letters(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency)
     end = time.time()
-    print(json.dumps(frequency, indent=4))
+    sorted_freq = dict(sorted(frequency.items()))
+    print(json.dumps(sorted_freq, indent=4))
     print("Done, time taken", end - start)
 
 
